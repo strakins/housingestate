d4a8fc3b6f83a4a425642b441cf3ad8f4731010c
strakins <akinolablessing25@gmail.com>
2023-10-18 10:22:40 +0100

    recent update

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js'
+dotenv.config()
+
+mongoose.connect(process.env.MONGDB_URI)
+.then(() => {
+    try {
+        console.log('Mongodb Connected')
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+const app = express();
+
+app.listen(3000, ()=> {
+    console.log(`Port Running on port 3000`);
+})
+
+
+app.use("/api/user", userRouter)
---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Routing from "./Routing"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      < Routing />
+    </>
+  )
+}
+
+export default App
